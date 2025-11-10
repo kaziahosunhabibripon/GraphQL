a6b268d59418e7a95207170c9bbd7d69d99613bc
kaziahosunhabibripon <habibkazi92@gmail.com>
2025-11-10 23:13:09 +0600

    many 2 many relations

--- types/BookType.js
+++ types/BookType.js
@@ -1,4 +1,6 @@
 import { GraphQLObjectType, GraphQLID, GraphQLString } from "graphql";
+import AuthorType from "./AuthorType.js";
+import Author from "../models/Author.js";
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -6,6 +8,12 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     title: { type: GraphQLString },
     authorId: { type: GraphQLID },
+    author: {
+      type: AuthorType,
+      resolve(parent) {
+        return Author.findById(parent.authorId);
+      },
+    },
   }),
 });
 export default BookType;
